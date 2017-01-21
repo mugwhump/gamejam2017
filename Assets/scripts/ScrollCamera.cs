304a5b3cf10f45d3d8b76526505a5d2ca5318c5a
Grayson Bartlet <grayson.bartlet@shaw.ca>
2017-01-21 15:43:55 -0700

    scrollable camera

---
+++ Assets/scripts/ScrollCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollCamera : MonoBehaviour {
+	public int scrollDistance; 
+	public float scrollSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mousePosX = Input.mousePosition.x; 
+		float mousePosY = Input.mousePosition.y; 
+		if (mousePosX < scrollDistance) { 
+			transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime); 
+		} 
+
+		if (mousePosX >= Screen.width - scrollDistance) { 
+			transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime); 
+		}
+
+		if (mousePosY < scrollDistance) { 
+			transform.Translate(Vector3.up * -scrollSpeed * Time.deltaTime); 
+		} 
+
+		if (mousePosY >= Screen.height - scrollDistance) { 
+			transform.Translate(Vector3.up * scrollSpeed * Time.deltaTime); 
+		}
+	}
+
+}
