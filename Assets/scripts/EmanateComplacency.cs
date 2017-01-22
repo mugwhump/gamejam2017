073cff9067a06389ffebe4a6d301e393f9764aad
Grayson Bartlet <grayson.bartlet@shaw.ca>
2017-01-21 18:44:42 -0700

    Made prefabs, adjusted sprite origins, made empty EmanateComplacency script

---
+++ Assets/scripts/EmanateComplacency.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Increases complacency of whatever collides with this object
+public class EmanateComplacency : MonoBehaviour {
+    public double rate; //complacency gained per second
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D other) {
+
+    }
+}
