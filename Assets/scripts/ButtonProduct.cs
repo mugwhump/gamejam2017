36331a2639b7b406527edec19c0e7972f384cf91
Grayson Bartlet <grayson.bartlet@shaw.ca>
2017-01-22 11:13:13 -0700

    Can build towers and fluoride factories

---
+++ Assets/scripts/ButtonProduct.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This behaviour is attached to a product (tower, fluoride plant, etc) while it's being dragged around
+//It disables all behaviors besides the sprite and waits for the user to click to place it somewhere, then 
+//re-enables everything and removes itself from the object.
+public class ButtonProduct : MonoBehaviour {
+    public ClickButton caller;
+
+	// Use this for initialization
+	void Awake () {
+        Debug.Log("ButtonProd here");
+        MonoBehaviour[] comps = GetComponents<MonoBehaviour>();
+        foreach (MonoBehaviour c in comps) {
+            if(c != this) c.enabled = false;
+        }
+        GetComponent<SpriteRenderer>().enabled = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(pos.x, pos.y, 0);
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("Placed product at "+pos.ToString());
+            caller.TaskOnSecondClick();
+            Destroy(this);
+        }
+	}
+
+    void OnDestroy() {
+        MonoBehaviour[] comps = GetComponents<MonoBehaviour>();
+        foreach (MonoBehaviour c in comps) {
+            c.enabled = true;
+        }
+    }
+    
+}
---
+++ Assets/scripts/ClickButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickButton : MonoBehaviour {
+    public GameObject product; //what this button creates
+    public int cost; //how much it costs
+    public double cooldown; //how long is the cooldown in seconds
+
+    private Button mButton;
+    private GameManager gm;
+    private double remainingTime; //how long until this can be clicked again
+
+	// Use this for initialization
+	void Start () {
+        mButton = GetComponent<Button>();
+        mButton.onClick.AddListener(TaskOnClick);
+        gm = GameObject.FindGameObjectWithTag("Canvas").GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(remainingTime > 0) {
+            remainingTime -= Time.deltaTime;
+        }
+	}
+
+    void TaskOnClick() {
+        Debug.Log("Button Task doin");
+        if (gm.money < cost) {
+            Debug.Log("YOU TOO POOR");
+        }
+        else if (remainingTime > 0) {
+            Debug.Log("STILL IN COOLDOWN");
+        }
+        else {
+            //charge them once they actually build it
+            GameObject prod = Instantiate(product);
+            ButtonProduct bp = prod.AddComponent<ButtonProduct>();
+            bp.caller = this;
+        }
+    }
+
+    public void TaskOnSecondClick() {
+        gm.money -= cost;
+        remainingTime = cooldown;
+    }
+}
